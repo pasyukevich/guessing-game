1f2814ef61104537c087b10446b27132d5597b38
pasyukevich <pasyukevich@live.com>
2017-01-31 00:57:59 +0300

    Add files via upload

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var min=0;
+        var max=0;
+    }
 
     setRange(min, max) {
-
+        this.min= min;
+        this.max= max;
     }
 
     guess() {
-
+        return (Math.round((this.min+this.max)/2))
     }
 
     lower() {
-
+        this.max=this.guess();
+        return this;
     }
 
     greater() {
-
+        this.min=this.guess();
+        return this;
     }
 }
 
